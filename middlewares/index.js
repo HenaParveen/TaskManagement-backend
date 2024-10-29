4009e90f2e851cb9567846f7ec6e8bbd2b88cedd
HenaParveen <138970020+HenaParveen@users.noreply.github.com>
2024-10-29 21:18:11 +0530

    Created common folder for error response & validation

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
   AuthMiddleware: require("./authMiddleware"),
-=======
-  AuthMiddleware: require("./authMiddleWare"),
->>>>>>> 5335afd8700a0c49e4d9c1e6df37f884d11de99f
 };
