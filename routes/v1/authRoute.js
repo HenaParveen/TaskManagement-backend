591a95b3682e53de14ed22cfbe3cfe2c6b029f5c
HenaParveen <138970020+HenaParveen@users.noreply.github.com>
2024-10-29 19:45:08 +0530

    Added Routing

--- routes/v1/authRoute.js
+++ routes/v1/authRoute.js
@@ -3,11 +3,8 @@ const { AuthController } = require("../../controllers");
 
 const { AuthMiddleware } = require("../../middlewares");
 
-/* to send the req to proper controllers */
-
 const router = express.Router();
 
-/* this is 4the level of routing till user */
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.signIn);
 router.post("/logout", AuthController.signOut);
